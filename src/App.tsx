485eeb8f581b2f733a01ac6b9eeed6057532a505
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-03-22 22:22:01 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactApp from './components/screens/ContactApp'
+
+function App() {
+  return (
+    <div>
+      <ContactApp/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ContactList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ContactList() {
+  return (
+    <div>ContactList</div>
+  )
+}
+
+export default ContactList
